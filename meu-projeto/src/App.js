017d838506179b2b968a95d8b06b4af6d5a0ed94
Fernando Venuto <fernandov.pereira@hotmail.com>
2022-10-13 15:16:13 -0300

    Curso React
    Comandos das aulas 1, 2, 3 , 4
    
    Aula 1: Conhecendo o projeto que será criando durante o curso.
    
    Aula 2: Instalando o React e Criando o arquivo
    
    Aula 3: Entendendo o JSX
    
    Aula 4: Componentes

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './components/HelloWorld'
 
 function App() {
+  const name = 'Fernando'
+  const newName = name.toUpperCase()
+  function sum(a, b) {
+    return a + b
+  }
+  const url = "https://via.placeholder.com/150"
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Alterando o JSX</h2>
+      <p>Olá, {newName} </p>
+      <p> soma: {sum(1, 2)}</p>
+      <img src={url} alt="Minha imagem" />
+      <HelloWorld />
     </div>
   );
 }
