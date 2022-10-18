bd90d177072dabf90210c5d0b94757d8f399cba7
Fernando Venuto <fernandov.pereira@hotmail.com>
2022-10-18 15:33:02 -0300

    Aulas 7 e 8

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
-import HelloWorld from "./components/HelloWorld";
 import SayMyName from "./components/SayMyName";
 import Pessoa from "./components/Pessoa";
 import Frase from "./components/Frase"
+import List from "./components/List"
 
 function App() {
   const nome = "Maria";
@@ -20,6 +20,7 @@ function App() {
         profissao="Programador"
         foto="https://via.placeholder.com/150"
       />
+      <List/>
     </div>
   );
 
